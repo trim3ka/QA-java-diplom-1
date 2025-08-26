93549016e8490dd50287857ab56e0a37d618d2a8
Marina Vovk <vovk-marina@yandex.ru>
2025-08-26 22:30:42 +0300

    - add BunTest class with unit tests - update pom.xml with Mockito and JUnit 5 dependencies

---
+++ src/test/java/praktikum/BunTest.java
@@ -0,0 +1,40 @@
+package praktikum;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(MockitoExtension.class)
+class BunTest {
+
+    @Mock
+    private Database database;
+
+    private Bun blackBun;
+    private Bun whiteBun;
+    private Bun redBun;
+
+    @BeforeEach
+    void setUp() {
+        // Инициализация объектов перед КАЖДЫМ тестом
+        blackBun = new Bun("black bun", 100);
+        whiteBun = new Bun("white bun", 200);
+        redBun = new Bun("red bun", 300);
+    }
+
+    @Test
+    void testBunNames() {
+        assertEquals("black bun", blackBun.getName());
+        assertEquals("white bun", whiteBun.getName());
+        assertEquals("red bun", redBun.getName());
+    }
+    @Test
+    void testBunPrices() {
+        assertEquals(100, blackBun.getPrice());
+        assertEquals(200, whiteBun.getPrice());
+        assertEquals(300, redBun.getPrice());
+    }
+}
